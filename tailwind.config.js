b897d6ff79367ed865226cb4e1c6377e40d5fbd7
Katia <hello@katiasmet.com>
2023-07-05 12:17:23 +0200

    fix: format and lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        "primary-color": "var(--primary-color)",
-        "secondary-color": "var(--secondary-color)"
+        'primary-color': 'var(--primary-color)',
+        'secondary-color': 'var(--secondary-color)',
       },
     },
   },
   plugins: [],
-}
-
+};
